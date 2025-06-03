85f327509712f16817cc9948ca3dc13e4a10b3a1
roskamsh <breesh.roskamshieter@gmail.com>
2025-06-03 14:40:02 +0100

    cleanup & add initial imputatin strategy

--- train_VAE.py
+++ train_VAE.py
@@ -9,10 +9,15 @@ from bin.helper_functions import get_scaled_data
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='config.json', help='path to configuration json file')
+parser.add_argument('--nextflow', type=bool, default=False, help = 'Whether you are running from a nextflow pipeline or not.')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    with open(args.config) as f:
+    configfile = args.config
+    run_nextflow = args.nextflow
+    configfile = "data/configs/test_params.json"
+    run_nextflow=False
+    with open(configfile) as f:
         config = json.load(f)
 
     model_settings = \
@@ -25,10 +30,12 @@ if __name__ == '__main__':
             beta = config["beta"],
             data_path = config["data_path"],
             corrupt_data_path = config["corrupt_data_path"]
-            )  
+            )
     
     # Set up and scale dataframes
-    data, data_missing = get_scaled_data(config["data_path"],config["corrupt_data_path"],nextflow=True)
+    data, data_missing = get_scaled_data(config["data_path"],config["corrupt_data_path"],
+                                         initial_imputation_strategy=config["initial_imputation_strategy"],
+                                         nextflow=run_nextflow)
     n_row = data.shape[1]
     model_settings['input_size']=n_row  # data input size
     
